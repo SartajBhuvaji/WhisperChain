168e541ede15c7967d3a69ede533b0759bf8f970
Sartaj Bhuvaji <s.bhuvaj@gmail.com>
2023-12-15 19:58:36 -0800

    _Pinecone bug

--- app.py
+++ app.py
@@ -41,23 +41,26 @@ def get_text_chunks(text):
 #     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl") if FREE_RUN else OpenAIEmbeddings()
 #     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
 #     return vectorstore
-
 def get_vector_store(text_chunks):
     # create vector store
     embeddings = HuggingFaceInstructEmbeddings(
         model_name="hkunlp/instructor-xl") if FREE_RUN else OpenAIEmbeddings()
 
     # Use Pinecone as the vector store
-    pinecone.init(api_key='PINECONE_API_KEY', environment='gcp-starter')
-    index = pinecone.Index("initial-index")
-    vectorstore = Pinecone(index, embeddings.embed_query, text_chunks)
-    # print("Vector store created")
-    # print(text_chunks)
+    pinecone.init(api_key=PINECONE_API_KEY, environment='gcp-starter')
+    index_name = "initial-index"  # replace with your desired index name
+    index = pinecone.Index(index_name)
+    
+    vectorstore = Pinecone(
+    index, embeddings.embed_query, text_chunks
+)
     return vectorstore
+
 
 def get_conversation_chain(vectorstore):
     # create conversation chain
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512}) if FREE_RUN else ChatOpenAI()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={
+                        "temperature": 0.5, "max_length": 512}) if FREE_RUN else ChatOpenAI()
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
@@ -65,6 +68,7 @@ def get_conversation_chain(vectorstore):
         memory=memory
     )
     return conversation_chain
+
 
 def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
@@ -78,9 +82,9 @@ def handle_userinput(user_question):
             st.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
 
-def main():
 
-    st.set_page_config(page_title="WhisperChain 🔗",page_icon=":link:")
+def main():
+    st.set_page_config(page_title="WhisperChain 🔗", page_icon=":link:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -98,7 +102,7 @@ def main():
         st.subheader("Your documents")
         pdf_docs = st.file_uploader(
             "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        
+
         if st.button("Process"):
             if pdf_docs:
                 with st.spinner("Processing"):
@@ -115,7 +119,8 @@ def main():
                     # create conversation chain
                     st.session_state.conversation = get_conversation_chain(vector_store)
             else:
-                st.error("Please upload at least one PDF")        
+                st.error("Please upload at least one PDF")
+
 
 if __name__ == '__main__':
     main()
